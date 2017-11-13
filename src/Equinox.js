508f64e0dc83db1d260e94a4c09f0d41be37aa09
Petr Jaroš <pjaros@samepage.io>
2017-11-13 07:40:40 +0100

    Update Equinox.js

--- src/Equinox.js
+++ src/Equinox.js
@@ -5,9 +5,7 @@ const julian = require('astronomia/lib/julian')
 const planetpos = require('astronomia/lib/planetposition')
 const earth = new planetpos.Planet(require('astronomia/data/vsop87Bearth'))
 
-const moment = require('moment-timezone')
 const CalEvent = require('./CalEvent')
-const CalDate = require('caldate')
 
 class Equinox extends CalEvent {
   /**
@@ -24,42 +22,6 @@ class Equinox extends CalEvent {
   }
 
   inYear (year) {
-    let jde
-    switch (this._season) {
-      case 'march': {
-        jde = solstice.march2(year, earth)
-        break
-      }
-      case 'june': {
-        jde = solstice.june2(year, earth)
-        break
-      }
-      case 'september': {
-        jde = solstice.september2(year, earth)
-        break
-      }
-      case 'december': {
-        jde = solstice.december2(year, earth)
-        break
-      }
-    }
-
-    const str = new julian.Calendar().fromJDE(jde).toDate().toISOString()
-    let date
-    if (/^[+-]\d{2}:\d{2}?$/.test(this._timezone)) { // for '+08:00' formats
-      date = moment(str).utcOffset(this._timezone)
-    } else { // for 'Asia/Shanghai' formats
-      date = moment(str).tz(this._timezone) // move to timezone
-    }
-
-    const floorDate = {
-      year: year,
-      month: date.month() + 1,
-      day: date.date()
-    }
-
-    const d = new CalDate(floorDate).setOffset(this.offset)
-    this.dates.push(d)
     return this
   }
 }
